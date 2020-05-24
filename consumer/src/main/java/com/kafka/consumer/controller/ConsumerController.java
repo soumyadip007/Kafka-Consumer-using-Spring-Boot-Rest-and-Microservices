fbb893915b921afb493bc183feb5735ed2d27c9c
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-24 20:13:02 +0530

    Consumer Confis

--- consumer/src/main/java/com/kafka/consumer/controller/ConsumerController.java
+++ consumer/src/main/java/com/kafka/consumer/controller/ConsumerController.java
@@ -3,12 +3,19 @@ package com.kafka.consumer.controller;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.kafka.consumer.entity.User;
+
 @Service
 public class ConsumerController {
 
-	@KafkaListener(topics="Kafka_String",)
+	@KafkaListener(topics="Kafka_String", groupId = "group_id")
 	public void ConsumeString(String msg) {
 		System.out.println(msg);
 	}
-	
+
+	@KafkaListener(topics = "Kafka_Object", groupId = "group_id",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
 }
