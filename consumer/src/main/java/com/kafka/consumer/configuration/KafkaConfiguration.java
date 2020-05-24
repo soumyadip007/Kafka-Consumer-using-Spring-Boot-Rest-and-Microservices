977512c67905f2d0afb7bc51d9ce4446a3eac401
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-24 20:08:41 +0530

    consumer added

--- consumer/src/main/java/com/kafka/consumer/configuration/KafkaConfiguration.java
+++ consumer/src/main/java/com/kafka/consumer/configuration/KafkaConfiguration.java
@@ -13,7 +13,7 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-import com.kafka.producer.entity.User;
+import com.kafka.consumer.entity.User;
 
 @EnableKafka
 @Configuration
