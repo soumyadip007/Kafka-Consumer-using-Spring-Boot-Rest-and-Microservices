742c8c25219333b9934bd055b84de5a6efa1f3ef
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-24 20:11:29 +0530

    Consumer Controller

--- consumer/src/main/java/com/kafka/consumer/controller/ConsumerController.java
+++ consumer/src/main/java/com/kafka/consumer/controller/ConsumerController.java
@@ -1,8 +1,14 @@
 package com.kafka.consumer.controller;
 
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConsumerController {
 
+	@KafkaListener(topics="Kafka_String",)
+	public void ConsumeString(String msg) {
+		System.out.println(msg);
+	}
+	
 }
