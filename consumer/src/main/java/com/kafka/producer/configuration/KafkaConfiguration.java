76912606ee9caf01ccdf79a787940940d7263c7f
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-24 20:04:23 +0530

    Kafka Config

--- consumer/src/main/java/com/kafka/producer/configuration/KafkaConfiguration.java
+++ consumer/src/main/java/com/kafka/producer/configuration/KafkaConfiguration.java
@@ -28,10 +28,12 @@ public class KafkaConfiguration {
 		
 		Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "Kafka_Object");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
+        
         return new DefaultKafkaProducerFactory<>(config);
     }
 	
