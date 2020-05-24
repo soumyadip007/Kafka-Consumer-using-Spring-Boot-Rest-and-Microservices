9a1b742d9995cf293a373bc5786cb3c487307600
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-24 20:03:57 +0530

    Kafka_Object

--- consumer/src/main/java/com/kafka/producer/configuration/KafkaConfiguration.java
+++ consumer/src/main/java/com/kafka/producer/configuration/KafkaConfiguration.java
@@ -3,7 +3,9 @@ package com.kafka.producer.configuration;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,7 +38,7 @@ public class KafkaConfiguration {
 	
 	@Bean
     public KafkaTemplate<String, User> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+        return new KafkaTemplate<>(consumerFactory());
     }
 	
 	//-----------------------String-String-----------------------
@@ -46,10 +48,13 @@ public class KafkaConfiguration {
 		
 		Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "Kafka_Object");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        
         return new DefaultKafkaProducerFactory<>(config);
     }
 	
